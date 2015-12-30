fb51c103645dc3e07ba16e394d95a4debcbaa051
Dong Nguyen <ducdong.mg@gmail.com>
2015-12-30 23:18:01 +0700

    Bài 5 $rootScrope - Biến dùng chung giữa các controller

--- app/app.js
+++ app/app.js
@@ -1,8 +1,7 @@
 var myMainApp = angular.module("myApp", []);
-myMainApp.controller('ShowMessage', function($scope) {
-	$scope.data = {'message' : 'xin chao', 'title' : 'tieu de'};
+myMainApp.controller('ShowMessage', function($scope, $rootScope) {
+	$rootScope.data = {'message' : 'xin chao', 'title' : 'tieu de', 'content' : 'content'};
 });
 
 myMainApp.controller('ShowContent', function($scope) {
-	$scope.data = {'content' : 'xin chao', 'title' : 'tieu de content'};
 });
