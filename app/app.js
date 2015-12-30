b372f7c7cd5078b0613cc368c10bfb4ae19f2ed6
Dong Nguyen <ducdong.mg@gmail.com>
2015-12-30 23:10:36 +0700

    Bài 4  Scope variable. Scope có phạm vi hoạt động trong controller chứa nó. 2 controller có cùng tên biến thì giá trị vẫn khác nhau giữa các controller

--- app/app.js
+++ app/app.js
@@ -2,3 +2,7 @@ var myMainApp = angular.module("myApp", []);
 myMainApp.controller('ShowMessage', function($scope) {
 	$scope.data = {'message' : 'xin chao', 'title' : 'tieu de'};
 });
+
+myMainApp.controller('ShowContent', function($scope) {
+	$scope.data = {'content' : 'xin chao', 'title' : 'tieu de content'};
+});
