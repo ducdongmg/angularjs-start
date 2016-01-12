c832cb82aa60be18e3a0befdea3e6283ed629d80
Dong Nguyen <ducdong.mg@gmail.com>
2016-01-12 23:18:06 +0700

    Bai 8 - Directive templateUrl

--- app/app.js
+++ app/app.js
@@ -1,17 +1,20 @@
-var myMainApp = angular.module("myApp", []);
-myMainApp.controller('ShowMessage', function($scope, $rootScope) {
-	$rootScope.data = {'message' : 'xin chao', 'title' : 'tieu de', 'content' : 'content'};
-});
-
-myMainApp.controller('ShowContent', function($scope) {
-	$scope.kq = 'ket qua la: ';
-	$scope.tinhkq = function(){
-		return $scope.kq += parseInt($scope.so1) + $scope.pheptoan + parseInt($scope.so2);
-	}
-});
-// define new directive
-myMainApp.directive("ngNewdirective", function () {
-	return {
-		template: "<b> Content </b>"
-	};
-} );
+(function(angular) {
+  'use strict';
+angular.module('docsTemplateUrlDirective', [])
+  .controller('Controller', ['$scope', function($scope) {
+    $scope.customer = {
+      name: 'Naomi',
+      password: 'Amphitheatre'
+    };
+  }])
+  .directive('myCustomer', function() {
+    return {
+      template: '<b>my-customer.html</b>'
+    };
+  })
+  .directive('myCustomerTemplate', function() {
+    return {
+      templateUrl: 'app/templates/login_form.html'
+    };
+  });
+})(window.angular);
